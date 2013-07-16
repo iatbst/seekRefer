197eff5419291130fdf6d3ef342db9a49632cffb
Jia Yang <jyang@02556a-jyang.corp.zynga.com>
2013-07-15 17:11:26 -0700

    chapter 6: user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,18 @@
 class User < ActiveRecord::Base
+  before_save do
+     self.email = email.downcase 
+     self.name = name.downcase
+  end
+  
   #belongs_to :company
+  #validation
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :name, presence: true, length: {maximum: 10, minimum: 5}, uniqueness: {case_sensitive: false}
+  validates :company, presence: true
+  validates :dept, presence: true
+  validates :position, presence: true
+  validates :email, presence: true, format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
+  
+  #password 
+  has_secure_password
 end
