391e8ffae9821666d727383d656c8928255f394c
JOSHUA <apple@applematoMacBook-Pro.local>
2013-07-22 01:16:46 -0700

    update

--- app/mailers/user_mailer.rb
+++
@@ -1,8 +0,0 @@
-class UserMailer < ActionMailer::Base
-  default from: "iatbst@gmail.com"
-  
-  def connect_request(user)
-    @user = user
-    mail(to: @user.email, subject: 'You have connection request!')
-  end
-end
